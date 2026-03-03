c8372c83448971874a7aad1a77022dca91a6d329
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-03 13:04:50 +0000

    Added RightSize project

---
+++ RightSize/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
