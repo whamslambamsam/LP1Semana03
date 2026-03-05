61ad5360b0be481e780e0d170df4f464cf7193b3
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-05 16:03:04 +0000

    Added enum variables

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -4,6 +4,24 @@ namespace CyberHack
 {
     public class Program
     {
+        public enum HackTool
+        {
+            BruteForce ,
+            Phishing ,
+            Backdoor ,
+            ZeroDay ,
+            AIOverride ,
+            QuantumExploit
+        }
+
+        public enum SystemType
+        {
+            CorporateServer ,
+            BankDatabase ,
+            SmartCityCore ,
+            MilitaryAI
+        }
+        
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
