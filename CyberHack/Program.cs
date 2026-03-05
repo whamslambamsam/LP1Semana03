ac4dea71a561ce96e6e277da5710314e69d13705
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-05 16:57:34 +0000

    Added inputs and Console.Realine() for each

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Runtime;
 
 namespace CyberHack
 {
     public class Program
     {
+        [Flags]
+        public enum SystemType
+        {
+            CorporateServer ,
+            BankDatabase ,
+            SmartCityCore ,
+            MilitaryAI
+        }
+
+        [Flags]
         public enum HackTool
         {
             BruteForce ,
@@ -14,17 +25,15 @@ namespace CyberHack
             QuantumExploit
         }
 
-        public enum SystemType
-        {
-            CorporateServer ,
-            BankDatabase ,
-            SmartCityCore ,
-            MilitaryAI
-        }
-        
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            // Tipo de sistema a atacar
+            Console.Write("");
+            string answerTarget = Console.ReadLine();
+            
+            // Ferramentas de hacking (uma ou mais)
+            Console.Write("");
+            string answerTools = Console.ReadLine();
         }
     }
 }
